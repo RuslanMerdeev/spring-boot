ad61d10b23a9bf40ccffbaeaf33c2ef791bef694
ruslanmerdeev <r.merdeev@haulmont.com>
2023-06-05 12:02:15 +0400

    [main] Added the simple controller

---
+++ src/main/kotlin/com/example/springBoot/controller/ActivityController.kt
@@ -0,0 +1,15 @@
+package com.example.springBoot.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/activity")
+@Suppress("unused")
+class ActivityController {
+
+    @PostMapping("/activate")
+    fun activate() {
+    }
+}
