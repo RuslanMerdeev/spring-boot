164dedee44eab6ad6399de1c8e4a69eb7907e251
ruslanmerdeev <r.merdeev@haulmont.com>
2024-09-22 20:10:16 +0300

    [flowable] draft

--- src/main/kotlin/com/example/springBoot/delegate/GetBrandDelegate.kt
+++ src/main/kotlin/com/example/springBoot/delegate/GetBrandDelegate.kt
@@ -4,17 +4,13 @@ import com.example.springBoot.service.ProductConfigService
 import com.example.springBoot.variable.BRAND
 import org.flowable.engine.delegate.DelegateExecution
 import org.flowable.engine.delegate.JavaDelegate
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Suppress("unused")
 class GetBrandDelegate(
     private val productConfigService: ProductConfigService,
 ): JavaDelegate {
-
-//    @Autowired lateinit var productConfigService: ProductConfigService
 
     override fun execute(execution: DelegateExecution) {
         val brand = productConfigService.fetchBy(execution.id).brand
