8d96df1731172b42bdf5167351f3e10769c7ba87
ruslanmerdeev <r.merdeev@haulmont.com>
2024-09-28 15:45:37 +0300

    [repo] draft

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$openapiVersion")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.3")
-    constraints {
-        implementation("commons-fileupload:commons-fileupload:1.5") {
-            because("version 1.4 pulled from spring-cloud-starter-openfeign has vulnerabilities")
-        }
-    }
     implementation("org.flowable:flowable-spring-boot-starter:6.8.1")
     implementation("com.h2database:h2:2.3.232")
 
